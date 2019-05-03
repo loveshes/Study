838906a0b54f74f2196b654cd2c3200ce2a5c6ee
LoveShes <loveshes@qq.com>
2019-05-03 21:57:14 +0800

    Update vim-config.sh

--- vim-config.sh
+++ vim-config.sh
@@ -4,12 +4,18 @@ echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
 sudo apt-get update
 sudo apt-get install -y curl
 sudo apt-get install -y git-core
+
+# 下载 vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-wget -q -P ~/ https://raw.githubusercontent.com/LoveShes/Study/master/.vimrc
-vim -c ":source ~/.vimrc" -c "PlugInstall" -c "qa"
+# 下载hybrid主题
 mkdir ~/.vim/colors/
-cp ~/.vim/plugged/vim-hybrid/colors/hybrid.vim ~/.vim/colors/hybrid.vim
-vim -c ":source ~/.vimrc" -c "qa"
+wget -q -P ~/.vim/colors/ https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid.vim
+
+# 下载.vimrc配置文件
+wget -q -P ~/ https://raw.githubusercontent.com/LoveShes/Study/master/.vimrc
+
+# 让vim插件生效
+vim -c "source ~/.vimrc" -c "PlugInstall" -c "qa"
 
 echo "安装完成"
