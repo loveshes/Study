4fabf95e7fbdba3e2d86d8d1f0deac4552d4e3af
LoveShes <loveshes@qq.com>
2019-05-03 21:45:32 +0800

    Update vim-config.sh

--- vim-config.sh
+++ vim-config.sh
@@ -7,8 +7,9 @@ sudo apt-get install -y git-core
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 wget -q -P ~/ https://raw.githubusercontent.com/LoveShes/Study/master/.vimrc
+vim -c ":source ~/.vimrc" -c "PlugInstall" -c "qa"
 mkdir ~/.vim/colors/
 cp ~/.vim/plugged/vim-hybrid/colors/hybrid.vim ~/.vim/colors/hybrid.vim
-vim -c ":source ~/.vimrc" -c "PlugInstall" -c "qa"
+vim -c ":source ~/.vimrc" -c "qa"
 
 echo "安装完成"
