0534efeff594a00a853e166d69d351ca7bc3b0d7
LoveShes <loveshes@qq.com>
2019-05-03 20:50:58 +0800

    Update vim-config.sh

--- vim-config.sh
+++ vim-config.sh
@@ -8,7 +8,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 echo -e "\" 显示行号\nset nu\n\n\" 语法高亮\nsyntax enable\nsyntax on\n" >> ~/.vimrc
 echo -e "\" 设置主题\nset background=dark\ncolorscheme hybrid\n" >> ~/.vimrc
 echo -e "\" 插件列表\ncall plug#begin('~/.vim/plugged')\nPlug 'mhinz/vim-startify'\nPlug 'vim-airline/vim-airline'\nPlug 'vim-airline/vim-airline-themes'\nPlug 'yggdroot/indentline'\nPlug 'w0ng/vim-hybrid'\ncall plug#end()\n" >> ~/.vimrc
-echo ":source ~/.vimrc" | vim
+
 vim -c ":source ~/.vimrc" -c "PlugInstall" -c "qa"
 
 echo "安装完成"
